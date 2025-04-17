c145d584f98557745bdc13795ea8c2c3a9a7b3f3
beenej <brandonbeene@outlook.com>
2025-04-16 17:31:19 -0700

    FibonacciDynamic

---
+++ FibonacciDynamic.js
@@ -0,0 +1,8 @@
+//memo = dynamic programming (dp) table
+const fib = (n, memo = {}) => {
+	if (n in memo) return memo[n];
+	if (n <= 2) return 1;
+	memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+	return memo[n];
+};
+console.log(fib(50));
